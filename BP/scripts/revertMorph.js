69ab8611384b6a3178b117bba5888dbf4d83e322
Zyko <micaiahejardin@gmail.com>
2026-06-10 17:32:32 -0500

    Add revertMorph.js to handle item use events

---
+++ BP/scripts/revertMorph.js
@@ -0,0 +1,13 @@
+import { world } from "@minecraft/server";
+
+world.afterEvents.itemUse.subscribe(event => {
+  const player = event.source;
+  const item   = event.itemStack;
+
+  if (item.typeId === "pack:reverter") {
+    player.triggerEvent("pa:player");
+
+    player.runCommand("replaceitem entity @s slot.armor.head 0 air 1");
+    player.runCommand("effect @s clear");
+  }
+}
